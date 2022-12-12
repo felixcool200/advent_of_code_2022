aff5f87963ad8373a52aeab831aeda56939c0a6d
Felix Söderman <felixsoderman@gmail.com>
2022-12-12 18:04:38 +0100

    Combined part A and B code

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,33 +1,14 @@
 use std::fs;
 use std::str::FromStr;
 
-
 fn main() {
     let file_path = "input.txt";
     let contents = fs::read_to_string(file_path)
         .expect("Should have been able to read the file");
     let list_of_str = contents.split("\n");
 
-    //Part A
-    let mut max_find:u64 = 0;
-    let mut current:u64 = 0;
-    
-    for str_num in list_of_str{
-        if str_num == ""{
-            if max_find < current{
-                max_find = current;
-            }
-            current = 0;
-        }else{
-            current += u64::from_str(str_num).unwrap_or(0);
-        }
-    }
-    let list_of_str = contents.split("\n");
-    println!("Max carry:{}",max_find);
-    
-
     //Part B
-    current = 0;
+    let mut current:u64 = 0;
     let mut max_array: [u64;3] = [0;3];
     for str_num in list_of_str{
         if str_num == ""{
@@ -45,5 +26,24 @@ fn main() {
     }
 
     let total:u64 = max_array.iter().sum();
-    println!("Total:{}",total);
+    println!("Part A:{}, Part B:{}",max_array[0],total);
+
+    /*
+    //Part A
+    let mut max_find:u64 = 0;
+    current = 0;
+    
+    for str_num in list_of_str{
+        if str_num == ""{
+            if max_find < current{
+                max_find = current;
+            }
+            current = 0;
+        }else{
+            current += u64::from_str(str_num).unwrap_or(0);
+        }
+    }
+    let list_of_str = contents.split("\n");
+    println!("Max carry:{}",max_find);
+    */
 }
