d26e9f02705043b86219ffcdc25ebeec4efe7331
felixcool200 <felixsoderman@gmail.com>
2022-12-17 14:58:47 +0100

    Refactoring

--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -1,84 +1,103 @@
 use std::fs;
 use std::collections::HashMap;
+use std::str::Split;
 
-fn find_priority(bags: &Vec<&str>,priority_map: &HashMap<char,usize>) -> usize{
+//Part A
+fn part_a(list_of_str:Split<&str>, priority_map:&HashMap<char,usize>) -> usize{
+    let mut sum:usize = 0;  
+    for str_num in list_of_str{
+        //Fix new line in input from VIM
+        if str_num == ""{
+            continue;
+        }
+        let len:usize = str_num.len()/2;
+        sum += match find_matching_letters(&str_num[..len],&str_num[len..]){
+            Ok(letter) => priority_map[&letter],
+            Err(err_msg) => {
+                println!("ERROR: {}",err_msg);
+                0
+            },
+        }
+    }  
+    return sum;
+}
+
+fn find_matching_letters(str1: &str,str2: &str) -> Result<char,&'static str>{
+    for letter in str1.chars(){
+        for letter2 in str2.chars(){
+            if letter == letter2{
+                return Ok(letter);
+            }
+        }
+    }
+    return Err("Could not find match");
+}
+
+//Part B
+fn part_b(list_of_str:Split<&str>, priority_map:&HashMap<char,usize>) -> usize{
+    let mut sum:usize = 0;  
+    let mut bags: Vec<&str> = Vec::new();
+    for str_num in list_of_str{
+        if str_num == ""{ //Igonre empty lines (VIM adds one to the end)
+            continue;
+        }
+        bags.push(str_num);
+        if bags.len() == 3{
+            sum += match find_badges(&bags){
+                Ok(letter) => priority_map[&letter],
+                Err(err_msg) => { 
+                    println!("ERROR: {}",err_msg);
+                    0
+                }
+            };
+            bags.clear();
+        }
+    }
+    return sum;
+    }
+
+fn find_badges(bags: &Vec<&str>) -> Result<char,&'static str>{
     for letter in bags[0].chars(){
         for letter2 in bags[1].chars(){
-            for letter3 in bags[2].chars(){
-                if letter == letter2 && letter == letter3{
-                    let priority = priority_map[&letter];
-                    println!("letter:{}, priority: {}",letter,priority);
-                    return priority;
+            if letter == letter2{
+                for letter3 in bags[2].chars(){
+                    if letter == letter3{
+                        return Ok(letter);
+                    }
                 }
             }
         }
     }
-    return 0;
+    return Err("No letter found");
 }
 
-fn main() {
+fn read_input(file_path: &str) -> String{
+    let contents:String = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+    return contents
+}
 
+fn create_hash_map() -> HashMap<char,usize>{
     let letters: &str = "abcdefghijklmnopqrstuvwxyz";
     let capital_letters: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     let mut priority_map: HashMap<char,usize> = HashMap::new();
     for (i,letter) in letters.chars().enumerate(){
         priority_map.insert(letter,i+1);
     }
-    for (i,letter) in capital_letters.chars().enumerate(){
+    for (i,letter ) in capital_letters.chars().enumerate(){
         priority_map.insert(letter,i+1+26);
     }
+    return priority_map;
+}
 
-    let file_path = "input.txt";
-    let contents = fs::read_to_string(file_path)
-        .expect("Should have been able to read the file");
-    let list_of_str = contents.split("\n");
+fn main() {
+    let file_path:&str = "input.txt";
+    let contents:String = read_input(file_path);
+    let priority_map:HashMap<char,usize> = create_hash_map();
+    let list_of_str:Split<&str> = contents.split("\n");
 
-    //Part A
-    let mut sum:usize = 0;  
-    for str_num in list_of_str{
-        let len:usize = str_num.len()/2;
-        let bag1: &str = &str_num[..len];
-        let bag2: &str = &str_num[len..];
-        for letter in bag1.chars(){
-            let last_sum:usize = sum;
-            for letter2 in bag2.chars(){
-                if letter == letter2{
-                    let priority = priority_map[&letter];
-                    //println!("letter:{}, priority: {}",letter,priority);
-                    sum += priority;
-                    break;
-                }
-            }
-            if last_sum != sum{
-                break;
-            }
-        }
-        //Fix new line in input from VIM
-        if str_num == ""{
-            continue;
-        }
-    }
-    //let list_of_str = contents.split("\n");
-    println!("Part A:{}",sum);
-    
-    //Part B
-    let list_of_str = contents.split("\n"); // Reseting the interator
-    let mut sum:usize = 0;  
-    let mut bags: Vec<&str> = Vec::new();
-    for str_num in list_of_str{
-        if bags.len() != 3{
-            if str_num == ""{
-                continue;
-            }
-            bags.push(str_num);
-        }else{
-            sum += find_priority(&bags,&priority_map);
-            bags.clear();
-            bags.push(str_num);
-        }
-        println!("input:{}, bags: {}",str_num,bags.len());
-    }
-    //let list_of_str = contents.split("\n");
-    println!("Part A:{}",sum);
+    println!("Part A:{}",part_a(list_of_str.clone(),&priority_map));
+    println!("Part B:{}",part_b(list_of_str.clone(),&priority_map));
+
 }
 
