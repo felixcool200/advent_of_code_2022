7bc55a5f61e0c3ad754d741d551ccea880592c59
Felix Söderman <felixsoderman@gmail.com>
2024-01-07 19:02:56 +0100

    Removed Target from day1

--- day_01/target/debug/day_01.d
+++
@@ -1,1 +0,0 @@
-/home/felixcool200/Documents/prog/Rust/advent_of_code_2022/day_01/target/debug/day_01: /home/felixcool200/Documents/prog/Rust/advent_of_code_2022/day_01/src/main.rs
--- day_01/target/debug/deps/day_01-d00b1985f923273f.d
+++
@@ -1,5 +0,0 @@
-/home/felixcool200/Documents/prog/Rust/advent_of_code_2022/day_01/target/debug/deps/day_01-d00b1985f923273f: src/main.rs
-
-/home/felixcool200/Documents/prog/Rust/advent_of_code_2022/day_01/target/debug/deps/day_01-d00b1985f923273f.d: src/main.rs
-
-src/main.rs:
